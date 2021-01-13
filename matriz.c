a9bdea156b41ce1e2baa20865f6b0d197a51ad10
adeluigi <ademario63@gmail.com>
2021-01-13 19:54:55 -0300

    matriz sem numero negativo

--- matriz.c
+++ matriz.c
@@ -1,58 +1,78 @@
 #include <time.h>
 #include <stdlib.h>
+#include <math.h>
 
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
+  int var = atoi(argv[1]);
+  
+  double potencia = pow(2, (double)(var)); 
+
+  int parametro = potencia;
+  printf("potencia %d",parametro);
+  printf("\n");
 
   srand(time(NULL));
 
-  int *firstMatrix[2000];
-  int *secondMatrix[2000];
-  int finalMatrix[2000][2000];
+  int *firstMatrix[parametro];
+  int *secondMatrix[parametro];
+  int finalMatrix[parametro][parametro];
   int some;
   int p1,p2;
 
-    for (int i=0; i<2000; i++){
-      firstMatrix[i] = (int *)malloc(2000 * sizeof(int));
+
+    for (int i=0; i<parametro; i++){
+      firstMatrix[i] = (int *)malloc(parametro * sizeof(int));
     }
 
-    for (int i=0; i<2000; i++){
-      secondMatrix[i] = (int *)malloc(2000 * sizeof(int));
+    for (int i=0; i<parametro; i++){
+      for (int z=0; z<parametro; z++){
+        firstMatrix[i][z] = (int *)malloc(parametro * sizeof(int));
+      }
+    }
+
+    for (int i=0; i<parametro; i++){
+      secondMatrix[i] = (int *)malloc(parametro * sizeof(int));
+    }
+
+    for (int i=0; i<parametro; i++){
+      for (int z=0; z<parametro; z++){
+        secondMatrix[i][z] = (int *)malloc(parametro * sizeof(int));
+      }
     }
 
 
 
-  for (int i = 0; i < 2000; i++)
+  for (int i = 0; i < parametro; i++)
   {
-    for (int j = 0; j < 2000; j++)
+    for (int j = 0; j < parametro; j++)
     {
       finalMatrix[i][j] = 0;
     }
     
   }
 
-  for (int i = 0; i < 2000; i++)
+  for (int i = 0; i < parametro; i++)
   {
-    for (int j = 0; j < 2000; j++)
+    for (int j = 0; j < parametro; j++)
     {
       firstMatrix[i][j] = rand() % 10;
     }
     
   }
 
-  for (int i = 0; i < 2000; i++){
-    for (int j = 0; j < 2000; j++){
+  for (int i = 0; i < parametro; i++){
+    for (int j = 0; j < parametro; j++){
       secondMatrix[i][j] = rand() % 10;
     }  
   }
 
-    for (int y = 0; y < 2000; y++)
+    for (int y = 0; y < parametro; y++)
     {
-      for (int x = 0; x < 2000; x++)
+      for (int x = 0; x < parametro; x++)
       {
 
-        for (int z = 0; z < 2000; z++)
+        for (int z = 0; z < parametro; z++)
         {
           finalMatrix[y][x] += firstMatrix[y][z]*secondMatrix[z][x];
         }
@@ -67,6 +87,13 @@ int main(int argc, char const *argv[])
     p2 = secondMatrix;
     p1 = NULL;
     p2 = NULL;
+
+  for (int y = 0; y < parametro; y++){
+    for (int x = 0; x < parametro; x++){
+      printf("%d ", finalMatrix[y][x]);
+    }  
+    printf("\n");
+  }
   
   
   return 0;
